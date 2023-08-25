53f65950313a18c3a121dff5b6d881e27180bed0
Sam Kennedy <sam@joinprospect.com>
2023-08-25 10:57:37 -0700

    Setup assessment

---
+++ src/app/api/createNote/route.ts
@@ -0,0 +1,13 @@
+import { createClient } from "@supabase/supabase-js";
+
+
+export async function POST(req) {
+    const supabase = createClient(
+        process.env.SUPABASE_URL,
+        process.env.SUPABASE_KEY
+      )
+    const data = await req.json()
+    const result = await supabase.from("Notes").insert([data])
+    console.log(result)
+    return Response.json({ message: "This Worked", success: true });
+}
